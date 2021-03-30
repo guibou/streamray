e5b0eb516bf84fdd5145a928c0b3817fd5d66c63
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2021-03-30 08:49:40 +0200

    Nixpkgs cleanup

--- default.nix
+++ default.nix
@@ -4,16 +4,16 @@
   # Explicit list of used files. Else there is always too much and
   # cache is invalidated.
   sources = pkgs.lib.sourceByRegex ./. [
-    ".*\.cabal$"
-    ".*\.hs$"
-    ".*\.md$"
+    ".*\\.cabal$"
+    ".*\\.hs$"
+    ".*\\.md$"
     "src"
     "app"
     "tests"
     "LICENSE"
   ];
 
-  shell = (app.envFunc { withHoogle = true;}).overrideAttrs (old: {
+  shell = (app.envFunc { withHoogle = true; }).overrideAttrs (old: {
     # The Haskell environment does not come with cabal-install
     nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.cabal-install ];
   });
@@ -24,18 +24,26 @@
       ++ [ pkgs.haskellPackages.haskell-language-server ];
   });
 
-  app = (pkgs.haskell.lib.buildFromSdist ((pkgs.haskellPackages.override {
+  haskellPackage = pkgs.haskell.packages.ghc8104.override {
     overrides = self: super: {
+      # used for asset loading
       wavefront = pkgs.haskell.lib.doJailbreak super.wavefront;
+      linear =
+        pkgs.haskell.lib.appendBuildFlag super.linear "--ghc-options=-haddock";
+      JuicyPixels = pkgs.haskell.lib.appendBuildFlag super.JuicyPixels
+        "--ghc-options=-haddock";
     };
-  }).callCabal2nix "streamray" sources { })).overrideAttrs (oldAttrs: {
+  };
+
+  app = (haskellPackage.callCabal2nix "streamray" sources {
+
+  }).overrideAttrs (oldAttrs: {
     buildInputs = oldAttrs.buildInputs;
     passthru = oldAttrs.passthru // { inherit shell shell_hls; };
   });
 
   formatting-fix = pkgs.mkShell {
-    nativeBuildInputs =
-      [ pkgs.haskellPackages.ormolu ];
+    nativeBuildInputs = [ pkgs.haskellPackages.ormolu ];
     shellHook = ''
       ormolu --mode inplace **/*.hs
       exit 0
