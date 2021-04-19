b3847f739f76f5c0414267c9a94da2fbd308b4ce
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2021-04-19 10:41:43 +0200

    Bugfix: compute the correct bounding box for transform

--- src/Streamray/Scene.hs
+++ src/Streamray/Scene.hs
@@ -5,13 +5,13 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE StandaloneDeriving #-}
 
 -- | Represents a static scene, a cornel box with one light
 module Streamray.Scene where
 
 import Control.DeepSeq
+import Data.Foldable
 import GHC.Generics
 import Streamray.Geometry.Box
 import Streamray.Geometry.Triangle
@@ -47,8 +47,13 @@ instance HasBoundingBox SceneGraph where
   toBox (SceneBVH bvh) = toBox bvh
   toBox (AttachMaterial _ o) = toBox o
   toBox (Transformed mat o) =
-    let Box pMin pMax = toBox o
-     in Box (transformPoint mat pMin) (transformPoint mat pMax)
+    let Box (P x y z) (P x' y' z') = toBox o
+     in foldl' addPointToBox mempty $ do
+          newX <- [x, x']
+          newY <- [y, y']
+          newZ <- [z, z']
+
+          pure $ transformPoint mat (P newX newY newZ)
 
 instance HasBoundingBox Geometry where
   toBox (Spheres o) = toBox o
